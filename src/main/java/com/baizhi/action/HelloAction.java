8bebe1fd6dffcd9867dff982526b2b6b1166ea29
shiyueshinigeidehuangyan <57441859+shiyueshinigeidehuangyan@users.noreply.github.com>
2019-11-06 20:34:43 +0800

    第一次修改

--- src/main/java/com/baizhi/action/HelloAction.java
+++ src/main/java/com/baizhi/action/HelloAction.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/hello")
 public class HelloAction {
 
+    //这是我在github上面做的修改 
+    
     @RequestMapping("/boot")
     @ResponseBody
     public  String   boot() throws Exception{
